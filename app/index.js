b83cddb11731cadcc58df71551cc29a7c9c6970f
Susmita-Dey <susmitadey475@gmail.com>
2023-04-22 12:47:06 +0530

    first commit

---
+++ app/index.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from 'expo-router'
+
+import { COLORS, icons, images, SIZES } from '../constants'
+import { NearbyJobs, PopularJobs, ScreenHeaderBtn, Welcome } from '../components'
+
+const Home = () => {
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn iconUrl={icons.user} dimension="100%" />
+                    ),
+                    headerTitle: ""
+                }} />
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{
+                    flex: 1,
+                    padding: SIZES.medium
+                }}>
+                    <Welcome />
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
+
+export default Home
---
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import styles from './screenheader.style'
+
+const ScreenHeaderBtn = ({ iconUrl, dimension }) => {
+  return (
+    <View style={styles}>
+      <Text>ScreenHeaderBtn</Text>
+    </View>
+  )
+}
+
+export default ScreenHeaderBtn
